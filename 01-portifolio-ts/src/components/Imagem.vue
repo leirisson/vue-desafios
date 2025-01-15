9ebd27c7753ad510a0634426c4228f6e9fb93f48
leirisson <103783610+leirisson@users.noreply.github.com>
2025-01-15 13:25:38 -0400

    ghhg

---
+++ 01-portifolio-ts/src/components/Imagem.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {}
+</script>
+
+
+
+<template>
+    <img src="http://github.com/leirisson.png" alt="foto do perfil"></img>
+</template>
+
+<style scoped>
+.img_perfil img {
+    border-radius: 1rem;
+    width: 100%;
+    border: 2px solid var(--branco);
+}
+</style>
